4d76d2bed2dc725f70d1dddd5b5eb8e79655828c
Дневник UNIX'оида <84270488+thm-unix@users.noreply.github.com>
2022-03-24 11:03:42 +0300

    v. 0.0.1

---
+++ installer.sh
@@ -0,0 +1,51 @@
+if [[ `whoami` == 'root' ]]; then
+	# Preventing from mess in current directory ;)
+	mkdir plainDE-tmp-src
+	cd plainDE-tmp-src
+
+
+	# Cloning all repos
+	git clone https://github.com/plainDE/plainPanel
+	git clone https://github.com/plainDE/plainAbout
+	#git clone https://github.com/plainDE/plainSettings
+
+
+	# Creating plainDE directory
+	mkdir /usr/share/plainDE
+
+
+	# Compiling plainPanel
+	cd plainPanel
+	qmake
+	make
+	cp plainPanel /usr/share/plainDE/
+	ln -s /usr/share/plainDE/plainPanel /usr/bin/plainPanel
+	cd ..
+
+	
+	# Compiling plainAbout
+	cd plainAbout
+	qmake
+	make
+	cp plainAbout /usr/share/plainDE/
+	ln -s /usr/share/plainDE/plainAbout /usr/bin/plainAbout
+	cd ..
+
+
+	# Compiling plainSettings
+	# ...
+
+
+	# Creating config
+	cd ..
+	mkdir -p ~/.config/plainDE
+	cp config.json ~/.config/plainDE/
+
+
+	# Removing temprorary files
+	rm -rf plainDE-tmp-src
+
+else
+	echo 'Run it as root.'
+fi
+
