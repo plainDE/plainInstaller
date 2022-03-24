0564019667b357bd399679771071e0c7e274d415
Дневник UNIX'оида <84270488+thm-unix@users.noreply.github.com>
2022-03-24 11:26:33 +0300

    v. 0.0.1

--- installer.sh
+++ installer.sh
@@ -1,56 +1,41 @@
 HOMEPATH=$HOME
 
-sudo su
+# Preventing from mess in current directory ;)
+mkdir plainDE-tmp-src
+cd plainDE-tmp-src
 
-#if [[ `whoami` == 'root' ]]; then
-	# Preventing from mess in current directory ;)
-	mkdir plainDE-tmp-src
-	cd plainDE-tmp-src
+# Cloning all repos
+git clone https://github.com/plainDE/plainPanel
+git clone https://github.com/plainDE/plainAbout
+#git clone https://github.com/plainDE/plainSettings
 
+# Creating plainDE directory
+sudo mkdir /usr/share/plainDE
 
-	# Cloning all repos
-	git clone https://github.com/plainDE/plainPanel
-	git clone https://github.com/plainDE/plainAbout
-	#git clone https://github.com/plainDE/plainSettings
-
-
-	# Creating plainDE directory
-	mkdir /usr/share/plainDE
-
-
-	# Compiling plainPanel
-	cd plainPanel
-	qmake
-	make
-	cp plainPanel /usr/share/plainDE/
-	ln -s /usr/share/plainDE/plainPanel /usr/bin/plainPanel
-	cd ..
-
+# Compiling plainPanel
+cd plainPanel
+qmake
+make
+sudo cp plainPanel /usr/share/plainDE/
+sudo ln -s /usr/share/plainDE/plainPanel /usr/bin/plainPanel
+cd ..
 	
-	# Compiling plainAbout
-	cd plainAbout
-	qmake
-	make
-	cp plainAbout /usr/share/plainDE/
-	ln -s /usr/share/plainDE/plainAbout /usr/bin/plainAbout
-	cd ..
+# Compiling plainAbout
+cd plainAbout
+qmake
+make
+sudo cp plainAbout /usr/share/plainDE/
+sudo ln -s /usr/share/plainDE/plainAbout /usr/bin/plainAbout
+cd ..
 
+# Compiling plainSettings
+# ...
 
-	# Compiling plainSettings
-	# ...
+# Creating config
+cd ..
+echo $HOMEPATH/.config/plainDE
+mkdir -p $HOMEPATH/.config/plainDE
+cp config.json $HOMEPATH/.config/plainDE/
 
-
-	# Creating config
-	cd ..
-	echo $HOMEPATH/.config/plainDE
-	mkdir -p $HOMEPATH/.config/plainDE
-	cp config.json $HOMEPATH/.config/plainDE/
-
-
-	# Removing temprorary files
-	rm -rf plainDE-tmp-src
-
-#else
-#     	echo 'Run it as root.'
-fi
-
+# Removing temprorary files
+rm -rf plainDE-tmp-src
