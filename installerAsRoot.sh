7181f30a4e495c6ad94acf0c5374075d8b0f24d1
Дневник UNIX'оида <84270488+thm-unix@users.noreply.github.com>
2023-04-29 23:38:04 +0300

    Update installerAsRoot.sh

--- installerAsRoot.sh
+++ installerAsRoot.sh
@@ -1,4 +1,4 @@
-RELEASEVER="0.5"
+RELEASEVER="0.5.1"
 echo "plainInstaller $RELEASEVER as Root"
 echo "==> Installing plainDE into your system."
 echo "::  Press enter to proceed or Ctrl-C to cancel."
